62a002eff879fccb462cb68fa1e680da1890a758
Anghel Andrei <andreinfs@yahoo.com>
2021-09-03 00:33:09 +0300

    Adding Providers and Listeners

---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/products_grid.dart';
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Products'),
+        ),
+        body: ProductsGrid());
+  }
+}
